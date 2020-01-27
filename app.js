238f42c3790c644d308448ab508059513acdb973
Samuel Antwi <antwisam80@yahoo.com>
2020-01-27 16:01:45 +0000

    added year of publish functionality

---
+++ app.js
@@ -0,0 +1,108 @@
+const form = document.querySelector('#book-form');
+const bookList = document.querySelector('#book-list');
+const table = document.querySelector('table');
+const errorMessage = document.querySelector('.error-message');
+const successMessage = document.querySelector('.success-message');
+const claerButt = document.querySelector('#clear-button');
+
+
+
+// Book class
+class Book{
+  constructor(title, author, isbn, year) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.year = year;
+  }
+  static showErrorMessage() {
+    const html = `
+    <p class="error">All input field required</p>
+    `;
+    errorMessage.innerHTML += html;
+  }
+  static showsuccessMessage() {
+    const html = `
+    <p class="success">Book added successfully</p>
+    `;
+    successMessage.innerHTML += html;
+  }
+}
+
+// Extending the book class
+class UI extends Book{
+  constructor(title, author, isbn, year){
+    super(title, author, isbn, year);
+  }
+  // add book method
+  addBookToList(book) {
+     const row = document.createElement('tr');
+     row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td>${book.year}</td>
+      <td><a class="delete-book" href="#">X</a></td>
+     `;
+     bookList.appendChild(row);
+     if(book.title) {
+       claerButt.classList.remove('class', 'd-none');
+     }
+  }
+
+  deleteBook(e) {
+    e.target.parentElement.parentElement.remove();
+  }
+
+  clearAllBooks() {
+    bookList.innerHTML = '';
+  }
+};
+
+
+// Add an event listener
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+  const year = document.getElementById('year').value;
+
+  // Instantiate the book
+  const book = new Book(title, author, isbn, year);
+  // Instantiate the ui class
+  const ui = new UI();
+  // Adding book
+  if(title === '' || author === '' || isbn === '' || year === '') {
+    Book.showErrorMessage();
+    setTimeout(() => {
+      errorMessage.innerHTML = '';
+    }, 2000);
+  } else {
+    ui.addBookToList(book);
+    Book.showsuccessMessage();
+    form.reset();
+    setTimeout(() => {
+      successMessage.innerHTML = '';
+    }, 2000);
+  }
+});
+
+// Deleting a Book from the UI.
+table.addEventListener('click', e => {
+  console.log(e.target.classList)
+  const ui = new UI();
+  if(e.target.classList.contains('delete-book')){
+    ui.deleteBook(e);
+  } 
+});
+
+// Clear All Books from UI
+claerButt.addEventListener('click', () => {
+  const ui = new UI();
+  if(confirm('Are you sure you want to clear all books?')) {
+    ui.clearAllBooks();
+    claerButt.classList.add('class', 'd-none');
+  }
+});
+
